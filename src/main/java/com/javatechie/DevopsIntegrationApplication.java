873254ab7e7c30ba1159a9e23318701454efe659
devops-shri <122585172+shriniwas-devops@users.noreply.github.com>
2023-04-07 19:48:18 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to Mr. DevOps Youtube Channel";
+		return "Sample web application ";
 	}
 
 	public static void main(String[] args) {
